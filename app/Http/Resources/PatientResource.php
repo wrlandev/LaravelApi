e51f9b93a49bb81123b675a78f61f25a928fcc8d
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-21 15:53:10 -0300

    fix(date/PatientResource): fixing the birthday

--- app/Http/Resources/PatientResource.php
+++ app/Http/Resources/PatientResource.php
@@ -15,12 +15,12 @@ class PatientResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'identify' => $this->id,
-            'full name' => $this->full_name,
-            'mother full name' => $this->full_name_mother,
-            'date of birth' => $this->date,
-            'document cpf' => $this->document_cpf,
-            'document cns' => $this->document_cns
+            'id' => $this->id,
+            'full_name' => $this->full_name,
+            'full_name_mother' => $this->full_name_mother,
+            'date_of_birth' => $this->date_of_birth,
+            'document_cpf' => $this->document_cpf,
+            'document_cns' => $this->document_cns
         ];
     }
 }
