ef8f00a58fe46ba34aa86191675b0581781f2365
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-21 12:52:08 -0300

    feat: patient model

---
+++ app/Models/Patient.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    protected $fillable = 
+    [
+        'full_name',
+        'full_name_mother',
+        'date_of_birth',
+        'document_cpf',
+        'document_cns'
+    ];
+
+    use HasFactory;
+}
