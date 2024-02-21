b176d33b5bafe9af24c5bdc8dcde46b4e30d6bb3
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-21 13:35:52 -0300

    feat: patient factory

---
+++ database/factories/PatientFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patient>
+ */
+class PatientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'full_name' => $this->faker->name,
+            'full_name_mother' => $this->faker->name,
+            'date_of_birth' => $this->faker->date,
+            'document_cpf' => $this->faker->numerify('###.###.###-##'),
+            'document_cns' => $this->faker->numerify('### #### #### ####')
+        ];
+    }
+}
