272222e563a9d2b58cd043c4d90fb74572b762ff
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-21 12:46:11 -0300

    feat: patients migration

---
+++ database/migrations/2024_02_21_153316_create_patients_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('full_name_mother');
+            $table->date('date_of_birth');
+            $table->string('document_cpf');
+            $table->string('document_cns');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
