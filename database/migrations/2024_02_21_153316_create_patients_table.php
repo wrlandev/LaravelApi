cf22b90a13a9899f2ef8f9453f4693b4b0c6cd7d
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-21 21:59:37 -0300

    feat(patient/migration): apply relations

--- database/migrations/2024_02_21_153316_create_patients_table.php
+++ database/migrations/2024_02_21_153316_create_patients_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->date('date_of_birth');
             $table->string('document_cpf');
             $table->string('document_cns');
+            $table->foreignId('addresses_id');
             $table->timestamps();
         });
     }
