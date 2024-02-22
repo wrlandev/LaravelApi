c50dee6a2eaf99d81ac5d4284644126619ac6e23
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-22 02:36:27 -0300

    fix(patient/address/migration) fix migrations

--- database/migrations/2024_02_22_002033_create_addresses_table.php
+++ database/migrations/2024_02_22_002033_create_addresses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('patients_id');
+            $table->foreignId('patient_id');
             $table->string('zipcode');
             $table->string('street');
             $table->string('complement');
