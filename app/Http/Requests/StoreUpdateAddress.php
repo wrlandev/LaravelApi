eeabf958d41ea846d17af043c8915e7ae068dd80
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-21 22:13:05 -0300

    feat: address validation

---
+++ app/Http/Requests/StoreUpdateAddress.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUpdateAddress extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = 
+        [
+            'zipcode' => 
+            [
+                'required',
+                'formato_cep'
+            ],
+
+            'street' => 
+            [
+                'required',
+                'min:3',
+                'max:33'
+            ],
+
+            'complement' => 
+            [
+                'required',
+                'min:3',
+                'max:66'
+            ],
+
+            'neighborhood' => 
+            [
+                'required',
+                'min:3',
+                'max:33'
+            ],
+
+            'city' => 
+            [
+                'required',
+                'min:3',
+                'max:33'
+            ],
+
+            'state' => 
+            [
+                'required',
+                'uf'
+            ],
+
+        ];
+
+        return $rules;
+    }
+}
